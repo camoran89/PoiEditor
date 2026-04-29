71d32768dc17e8ccb2a11d6572757bc61ab8edc9
camilo_moran <camilo_moran@satrack.com>
2026-04-29 10:54:13 -0500

    feat(core): add POI_CATEGORIES constant and PoiCategory branded type

---
+++ src/app/core/constants/poi-categories.const.ts
@@ -0,0 +1,14 @@
+export const POI_CATEGORIES = [
+  'landmark',
+  'park',
+  'restaurant',
+  'cafe',
+  'museum',
+  'hotel',
+  'hospital',
+  'school',
+  'shopping',
+  'transport',
+  'sport',
+  'other',
+] as const;
---
+++ src/app/core/types/poi-category.type.ts
@@ -0,0 +1,3 @@
+import { POI_CATEGORIES } from '../constants/poi-categories.const';
+
+export type PoiCategory = (typeof POI_CATEGORIES)[number];
